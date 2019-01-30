7587a26f5c6216853dae374efea79d126e6803af
Daniel Gomes <daniel.g@beemoov.com>
2019-01-30 16:20:53 +0100

    Créa class mongoDbSingleton

---
+++ lib/core/class/mongoDbSingleton.php
@@ -0,0 +1,82 @@
+<?php
+namespace core;
+
+/**
+ * Connexion aux bases de données MongoDb
+ *
+ * @author Daniel Gomes
+ */
+class mongoDbSingleton
+{
+	/**
+	 * Attributs
+	 */
+	private static $instance = array();
+
+
+	/**
+	 * Création d'une instance MongoDb si elle n'existe pas déjà
+	 */
+	public static function getInstance($name='default', $options=array(), $timeZone='Europe/Paris')
+	{
+		if (! array_key_exists($name, self::$instance))
+		{
+			// Déclaration du fuseau horaire et récupération du décalage horaire
+			// date_default_timezone_set($timeZone);
+	        // $dateTimeZone   = new \DateTimeZone($timeZone);
+	        // $dateTime       = new \DateTime("now", $dateTimeZone);
+	        // $gmt = '+0' . ($dateTimeZone->getOffset($dateTime) / 3600) . ':00';
+
+			// Connexion à la base de données
+			$mongo = config::getConfig('mongo');
+
+			$mongoHost = $mongo[$name]['mongoHost'];
+			$mongoUser = $mongo[$name]['mongoUser'];
+			$mongoPass = $mongo[$name]['mongoPass'];
+			$mongoPort = $mongo[$name]['mongoPort'];
+
+			// $charset = $db[$name]['charset'];
+
+
+			try {
+				// Récupération de la configuration de connexion MongoDb
+				$mongo = config::getConfig('mongo');
+
+				$mongoHost = $mongo[$name]['mongoHost'];
+				$mongoUser = $mongo[$name]['mongoUser'];
+				$mongoPass = $mongo[$name]['mongoPass'];
+				$mongoPort = $mongo[$name]['mongoPort'];
+
+				// Options de connections
+				$defaultOptions = array("socketTimeoutMS" => 1000000);
+				$connectOptions = array_merge($defaultOptions, $options);
+
+				// Connexion à la base de données
+			    $connect = '';
+			    if (!empty($mongoUser) && !empty($mongoPass)) {
+			        $connect = $mongoUser . ':' . $mongoPass . '@';
+			    }
+
+			    self::$instance[$name] = new Mongo(
+			        'mongodb://' . $connect . $mongoHost . ':' . $mongoPort,
+			        $connectOptions
+			    );
+
+			} catch (\Exception $e) {
+
+			    echo $e->getMessage();
+			}
+		}
+
+		return self::$instance[$name];
+	}
+
+
+	/**
+	 * Fermeture d'une instance PDO
+	 */
+	public static function closeInstance($name='default')
+	{
+		unset(self::$instance[$name]);
+	}
+}
